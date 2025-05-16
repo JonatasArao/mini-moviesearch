3cae7fe4b39f905c3d4822126295d293057060c2
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-05-16 14:07:49 -0300

    refactor: reorganize the HTML structure and improve the pagination logic in JavaScript

--- js/app.js
+++ js/app.js
@@ -3,22 +3,44 @@ import './components/movieList.js';
 
 let currentSearchId = 0;
 
-document.getElementById('search-form').addEventListener('submit', function(e) {
-	e.preventDefault();
-	const searchQuery = document.getElementById('search-input').value;
+const homeTitle = document.getElementById('home-title');
+const searchForm = document.getElementById('search-form');
+const pagination = document.getElementById('pagination')
+const prevPage = document.getElementById('prev-page');
+const nextPage = document.getElementById('next-page');
+const pageInfo = document.getElementById('page-info');
+
+let totalPages;
+let currentPage;
+
+homeTitle.addEventListener('click', function(e) {
+	const movieList = document.getElementById('movie-list');
+	currentPage = 1;
+	movieList.movies = [];
+	pagination.hidden = true;
+	pageInfo.textContent = '';
+});
+
+function updateMovieList(searchQuery, page, updateTotalPages = false) {
 	const movieList = document.getElementById('movie-list');
 	movieList.loading = true;
 	movieList.error = '';
-
 	currentSearchId++;
 	const searchId = currentSearchId;
-	searchMovies(searchQuery)
+	searchMovies(searchQuery, page)
 		.then(res => {
 			if (searchId !== currentSearchId) return;
-			if (res.total_results == 0) {
+			if (updateTotalPages && res.total_results == 0) {
 				throw new Error('No results found');
 			}
 			movieList.movies = res.results;
+			if (updateTotalPages) {
+				totalPages = res.total_pages;
+			}
+			pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
+			prevPage.disabled = currentPage === 1;
+			nextPage.disabled = currentPage === totalPages;
+			pagination.hidden = undefined;
 		})
 		.catch((err => {
 			movieList.error = err.message;
@@ -26,4 +48,27 @@ document.getElementById('search-form').addEventListener('submit', function(e) {
 		.finally(() => {
 			movieList.loading = false;
 		});
-})
+}
+
+searchForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	const searchQuery = document.getElementById('search-input').value;
+	currentPage = 1;
+	updateMovieList(searchQuery, currentPage, true);
+});
+
+prevPage.addEventListener('click', function(e) {
+	if (currentPage > 1) {
+		currentPage--;
+		const searchQuery = document.getElementById('search-input').value;
+		updateMovieList(searchQuery, currentPage);
+	}
+});
+
+nextPage.addEventListener('click', function(e) {
+	if (currentPage < totalPages) {
+		currentPage++;
+		const searchQuery = document.getElementById('search-input').value;
+		updateMovieList(searchQuery, currentPage);
+	}
+});
